d9d2cb83c08998b4a8688131ff4686107eeea83e
Scott Bale <scott.bale@datastax.com>
2020-12-10 19:56:16 -0600

    remove unused dep

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject twitter-purge "0.0.1"
   :description "Delete most of my tweets"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v20.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.cli "1.0.194"]
-                 [org.clojure/tools.nrepl "0.2.13"]
                  [org.clojure/tools.logging "1.1.0"]
                  [clj-http "3.10.3" :exclusions [commons-codec]]
                  [commons-codec "1.14"]
